eb4c97645df1c6b7603f85f284edd7165bc35d27
Benjamin345 <benjamin.charpentier@devinci.fr>
2018-03-02 19:14:06 +0100

    récupération du nombre d'étoiles

--- michelin.js
+++ michelin.js
@@ -3,10 +3,10 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-
+var i;
 app.get('/scrape', function(req, res){
 
-	var url= 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
+	var url= 'https://restaurant.michelin.fr/restaurants/france/restaurants-3-etoiles-michelin';
 
 	request(url, function(error, response, html){
 		
@@ -19,50 +19,43 @@ app.get('/scrape', function(req, res){
 				   	
 				    //get the name of the restaurant
 			    	$('.view-mode-poi_card').each(function(i,element){
-			    		var restaurants = { 'title' : '','offers' : '','cuisine' :'' , 'price' : ''};
+			    		var restaurants = { 'title' : '','offers' : '','cuisine' :'' , 'price' : '','stars':''};
 
-			    		var data = $(this).children().first();
+			    		var data = $(this);
 
-			    		var details = data.children('.poi_card-details').children('.poi_card-description');
-			    		var info = data.children('.poi_card-details').children('.poi_card-other-info').first().children().first().children().first();
+			    		var stars = data.find('.guide').children().first();
+			    		if(stars.attr('class')=="guide-icon icon-mr icon-cotation1etoile"){
+			    			restaurants.stars='1 etoile';
+			    		}
+			    		else if(stars.attr('class')=="guide-icon icon-mr icon-cotation2etoiles"){
+			    			restaurants.stars='2 etoiles';
+			    		}
+			    		else if(stars.attr('class')=="guide-icon icon-mr icon-cotation3etoiles"){
+			    			restaurants.stars='3 etoiles';
+			    		}
+			    		
 
-				        var title = details.children('.poi_card-display-title').text();        
+				        var title = data.find('.poi_card-display-title').text();        
 				        restaurants.title = title.trim();
 				        			    	 	
-		    	 		var offers = details.children('.poi_card-display-offers').first().text();
+		    	 		var offers =  data.find('.poi_card-display-offers').first().text();
 		    	 		restaurants.offers=offers.trim(); 			    	 	
 		    		
-				        var cuisine = info.children('.poi_card-display-cuisines').text();        
+				        var cuisine =  data.find('.poi_card-display-cuisines').text();        
 				        restaurants.cuisine = cuisine.trim();
 
-				        var price = info.children('.poi_card-display-price').text();        
+				        var price =  data.find('.poi_card-display-price').text();        
 				        restaurants.price = price.trim();
 				        
 			    	 	js[key].push(restaurants);
 			    	 	})
 				 
-			    	 	fs.writeFile('output.json', JSON.stringify(js, null, 4),function(err){
+			    	 	fs.writeFile('Liste-restaurants.json', JSON.stringify(js, null, 4),function(err){
 			    });
 			    	
-			    	
-				    //get the offers of the restaurant
-				  
-				}
-			// To write to the system we will use the built in 'fs' library.
-			// In this example we will pass 3 parameters to the writeFile function
-			// Parameter 1 :  output.json - this is what the created filename will be called
-			// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-			// Parameter 3 :  callback function - a callback function to let us know the status of our function
-
-				
-
-				   
-
-				// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-				
-			
+			}
 		
-		res.send('Check your console!')	
+		res.send('A file Liste-restaurants.json has been created in your working directory!')	
 	});
 })
 
